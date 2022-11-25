4f80b13b13d309b92ca39732c5eee30b3f13b6d2
AbdullahRedoan27 <abdullahredoan8@gmail.com>
2022-11-26 00:05:59 +0600

    category div done

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function run() {
   });
   const usersCollection = client.db('4wheelanes').collection('users');
   const carsCollection = client.db('4wheelanes').collection('cars');
+  const categoriesCollection = client.db('4wheelanes').collection('carCategories')
 
   try {
     app.post('/users', async(req, res)=> {
@@ -32,6 +33,20 @@ function run() {
         const query = {email:email};
         const cursor =await usersCollection.findOne(query);
         res.send(cursor);
+    })
+
+    app.get('/allProducts', async(req, res) => {
+        const query = {};
+        const result = await carsCollection.find(query).toArray();
+        res.send(result)
+    })
+
+        app.get('/products/:category', async(req, res) => {
+        const category = req.params.category;
+        const query = {category: category,
+                        status: 'Available'};
+        const result = await carsCollection.find(query).toArray();
+        res.send(result)
     })
 
     app.post('/sellCar', async(req, res) => {
@@ -49,10 +64,67 @@ function run() {
 
     app.get('/dashboard/productDetails/:id', async(req, res) => {
         const id = req.params.id;
-        console.log(id);
         const query = {_id: ObjectId(id)};
         const result =await carsCollection.findOne(query);
         res.send(result)
+    })
+
+    app.put('/changeStatus', async(req, res) => {
+        const id = req.query.id
+        console.log(id);
+        const query = {_id: ObjectId(id)}
+        const options = {upsert: true};
+        const updatedDoc = {
+            $set: {
+                status: "Sold"
+            }
+        }
+        const result = await carsCollection.updateMany(query, updatedDoc, options)
+        res.send(result)
+    })
+
+    app.delete('/deleteProduct', async(req, res)=> {
+        const id = req.query.id;
+        const query = {_id: ObjectId(id)}
+        const result = await carsCollection.deleteOne(query);
+        res.send(result);
+    })
+
+    app.get('/dashboard/productDetails/editProduct/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result =await carsCollection.findOne(query);
+        res.send(result);
+    })
+
+    app.put('/updateCarDetails', async(req, res)=>{
+        const id = req.query.id;
+        console.log(id);
+        const query = {_id: ObjectId(id)};
+        const updatedCarDetails = req.body;
+        const options = {upsert:true}
+        const carDetails = await carsCollection.findOne(query);
+        const updatedDoc = {
+            $set:{
+                carName: updatedCarDetails?.carName,
+                category: updatedCarDetails?.category,
+                image: updatedCarDetails?.image,
+                carDetails: updatedCarDetails?.carDetails,
+                location: updatedCarDetails?.location,
+                usingPeriod: updatedCarDetails?.usingPeriod,
+                quality: updatedCarDetails?.quality,
+                originalPrice: updatedCarDetails?.originalPrice,
+                resalePrice: updatedCarDetails?.resalePrice
+            }
+        }
+        const result = await carsCollection.updateOne(query, updatedDoc, options)
+        res.send(result);
+    })
+
+    app.get('/categories', async(req, res) => {
+        const query = {};
+        const result = await categoriesCollection.find(query).toArray();
+        res.send(result);
     })
 
   } 
