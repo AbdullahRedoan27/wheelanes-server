3358faa4c38407536fc4c89064d28a0fa4107548
AbdullahRedoan27 <abdullahredoan8@gmail.com>
2022-11-30 01:48:53 +0600

    jwt done

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ function run() {
       res.send(result);
     });
 
-    app.get("/dashboard/productDetails/editProduct/:id", verifyJWT, async (req, res) => {
+    app.get("/dashboard/productDetails/editProduct/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await carsCollection.findOne(query);
